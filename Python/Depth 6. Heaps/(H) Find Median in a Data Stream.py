1093b54ff9f1de81b30039c66e5a8f6471d0b77f
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2024-07-15 07:57:48 -0500

    Reviewed NC soln to (H) Find Median in a Data Stream.py

--- Python/Depth 6. Heaps/(H) Find Median in a Data Stream.py
+++ Python/Depth 6. Heaps/(H) Find Median in a Data Stream.py
@@ -50,3 +50,37 @@ class MedianFinder:
         return self.buf[0]
         
 
+
+# NeetCode Solution
+# He used 2 heaps as well, but instead of overthinking what I needed to do
+# to handle ordering, he just logically thought out each possible case of
+# what could happen to the data structures and what logic to run at each step.
+class NeetCodeMedianFinder:
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        # two heaps, large, small, minheap, maxheap
+        # heaps should be equal size
+        self.small, self.large = [], []  # maxHeap, minHeap (python default)
+
+    def addNum(self, num: int) -> None:
+        if self.large and num > self.large[0]:
+            heapq.heappush(self.large, num)
+        else:
+            heapq.heappush(self.small, -1 * num)
+
+        if len(self.small) > len(self.large) + 1:  # left side too big
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:  # right side too big
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small):
+            return self.large[0]
+        return (-1 * self.small[0] + self.large[0]) / 2.0
+
