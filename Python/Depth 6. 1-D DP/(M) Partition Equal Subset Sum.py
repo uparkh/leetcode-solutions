87e9584747572e7fce76970227463ac1739493fc
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2024-08-03 15:52:31 -0500

    Did (M) Partition Equal Subset Sum

--- Python/Depth 6. 1-D DP/(M) Partition Equal Subset Sum.py
+++ Python/Depth 6. 1-D DP/(M) Partition Equal Subset Sum.py
@@ -33,3 +33,47 @@ class Solution:
         return left_sum == right_sum
         
 
+# NeetCode-Inspired Solution
+# Time: O(n * sum(nums))
+# Space : O(n * sum(nums))
+# Wow, I actually arrived at the set solution and didn't realize it was
+# optimal! I guess I really just needed to trust myself more...
+# I keep robbing myself of the victory feeling by not even attempting
+# to implement any solution I find.
+# New policy: Implement whichever solution I find, even if not optimal 
+# initially. Optimize later.
+class ModdedSolution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+        target = total // 2
+        dp = {0, }
+        for n in nums:
+            for s in list(dp):
+                if n + s == target:
+                    return True
+                dp.add(n + s)
+        return False
+
+
+# NeetCode Solution
+class NeetCodeSolution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
+
+        for i in range(len(nums) - 1, -1, -1):
+            nextDP = set()
+            for t in dp:
+                if (t + nums[i]) == target:
+                    return True
+                nextDP.add(t + nums[i])
+                nextDP.add(t)
+            dp = nextDP
+        return False
+
